18f24bd950794d6f4866670cc5c2e74caa302381
andreluismoreira <andreluistma@gmail.com>
2021-10-29 12:36:28 -0300

    Criação da animação de recolher sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import * as jQuery from 'jquery'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'AppClients';
+
+  ngAfterViewInit(){
+    (function($) {
+      "use strict";
+  
+      $("#sidebarToggle").on("click", function(e) {
+          e.preventDefault();
+          $("body").toggleClass("sb-sidenav-toggled");
+      });
+  })(jQuery);
+  }
+  
 }
