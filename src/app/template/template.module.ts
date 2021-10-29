676f33a668128fb6022a6c5fba23d0ad6caa3385
andreluismoreira <andreluistma@gmail.com>
2021-10-29 12:56:37 -0300

    Criação do Footer e do Home da Spa

--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -2,19 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule
   ],
   exports:[
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    FooterComponent,
+    HomeComponent
   ]
 })
 export class TemplateModule { }
