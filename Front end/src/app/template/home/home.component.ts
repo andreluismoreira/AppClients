c21814a0c3bb65ee679a9bd417de42de276c07c0
andreluismoreira <andreluistma@gmail.com>
2021-12-23 21:58:12 -0300

    melhoria no home

--- Front end/src/app/template/home/home.component.ts
+++ Front end/src/app/template/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,12 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent  {
 
-  constructor() { }
+  usuario!:string;
+
+  constructor(private service: AuthService) { }
 
   ngOnInit(): void {
+    this.usuario = this.service.getusuarioAutenticado();
   }
 
 }
